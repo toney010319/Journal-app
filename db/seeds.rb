86c8dfb88d99a3d3d02d7589128f9d5826a7bff5
Toney010319 <toney010319@gmail.com>
2023-12-02 10:41:03 +0800

    Adding CRUD in Category

--- db/seeds.rb
+++ db/seeds.rb
@@ -7,3 +7,10 @@
 #   ["Action", "Comedy", "Drama", "Horror"].each do |genre_name|
 #     MovieGenre.find_or_create_by!(name: genre_name)
 #   end
+
+Category.create(name: "Work", description: "Organize tasks related to work or professional responsibilities.")
+Category.create(name: "Personal Goals", description: "Keep track of personal goals and milestones.")
+Category.create(name: "Shopping List", description: "Manage items to buy during shopping trips.")
+Category.create(name: "Fitness Routine", description: "Plan and monitor your fitness activities and workouts.")
+Category.create(name: "Project Ideas", description: "Store and organize ideas for future projects or activities.")
+ 
