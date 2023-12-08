9c5edd6b3fcfa19a05a0f42e085d5c777d85b758
Toney010319 <toney010319@gmail.com>
2023-12-08 11:36:15 +0800

    Create Test in Pages_controller

--- test/controllers/pages_controller_test.rb
+++ test/controllers/pages_controller_test.rb
@@ -1,6 +1,26 @@
 require "test_helper"
 
+# class PagesControllerTest < ActionDispatch::IntegrationTest
+#   test "should get home" do
+#     get categories_path
+#     assert_response :success
+#   end
+
+#   test "should get about" do
+#     get root_path
+#     assert_response :success
+#   end
+# end
+require "test_helper"
+
 class PagesControllerTest < ActionDispatch::IntegrationTest
+  include Devise::Test::IntegrationHelpers
+
+  setup do
+    @user = users(:one) # Assuming you have a users fixture
+    sign_in @user
+  end
+
   test "should get home" do
     get categories_path
     assert_response :success
